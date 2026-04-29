963f5fee04167f07116a30b2b35e3a2518e8d072
zz2548 <zz2548@nyu.edu>
2026-04-29 11:17:01 -0400

    Update main.gd

--- underwater-game/main.gd
+++ underwater-game/main.gd
@@ -2,8 +2,8 @@
 extends Node
 
 func _ready() -> void:
-	# get_tree().change_scene_to_file.call_deferred("res://cutscene/cutscene.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://cutscene/cutscene.tscn")
 	
 	# get_tree().change_scene_to_file.call_deferred("res://levels/level_1/level_1.tscn")
 
-	get_tree().change_scene_to_file.call_deferred("res://levels/level_2/level_2.tscn")
+	# get_tree().change_scene_to_file.call_deferred("res://levels/level_2/level_2.tscn")
